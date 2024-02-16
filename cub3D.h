bb01d5ca654ab6ba3f5e596f5bc5322cab06e8ad
Yuka Itoh <yitoh@f0r2s11.codam.nl>
2024-02-16 15:52:47 +0100

    hfile updated2

---
+++ cub3D.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   cub3D.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: yitoh <yitoh@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/02/16 15:51:41 by yitoh         #+#    #+#                 */
+/*   Updated: 2024/02/16 15:52:36 by yitoh         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+// Headers
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <errno.h>
+
+
+#endif
