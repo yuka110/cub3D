d2f67e1f389d3abaa4dd2400434bc8d2f3cc30ec
Elena Voronin <evoronin@f0r2s13.codam.nl>
2024-03-01 16:59:19 +0100

    init_screen update

--- include/mlx_lib.h
+++ include/mlx_lib.h
@@ -6,7 +6,7 @@
 /*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/27 12:59:06 by evoronin      #+#    #+#                 */
-/*   Updated: 2024/02/29 17:24:16 by elenavoroni   ########   odam.nl         */
+/*   Updated: 2024/03/01 16:39:35 by evoronin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,22 @@ typedef struct s_data
 {
 	mlx_t		*mlx;
 	mlx_image_t	*img;
-	int32_t		width;
-	int32_t		height;
-	int32_t		y;
-	int32_t		x;
+	t_map		*map;
+	double		pos_x;
+	double		pos_y;
+	double		dir_x;
+	double		dir_y;
+	double		plane_x;
+	double		plane_y;
+	double		time;
+	double		old_time;
 }	t_data;
 
 //init_screen
 int		init_screen(t_map *map);
-
+void	init_loop(t_data *data);
+void	ft_hooks(mlx_key_data_t k, void *param);
+void	cast_ray(t_data *data);
+void	cast_ray_next(t_data *data);
 
 #endif
--- src/init_screen.c
+++ src/init_screen.c
@@ -6,30 +6,86 @@
 /*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/27 11:41:46 by evoronin      #+#    #+#                 */
-/*   Updated: 2024/03/01 14:39:21 by evoronin      ########   odam.nl         */
+/*   Updated: 2024/03/01 16:41:16 by evoronin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 #include "../include/mlx_lib.h"
 
-// int	init_loop()
-// {
-// 	while ()
-// }
+void	cast_ray_next(t_data *data)
+{
+	int mapX = int(posX);
+	int mapY = int(posY);
+	  //length of ray from current position to next x or y-side
+	double sideDistX;
+	double sideDistY;
+	   //length of ray from one x or y-side to next x or y-side
+	double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
+	double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
+	double perpWallDist;
+	  //what direction to step in x or y-direction (either +1 or -1)
+	int stepX;
+	int stepY;
+	int hit = 0; //was there a wall hit?
+	int side; //was a NS or a EW wall hit?
+}
+
+void	cast_ray(t_data *data)
+{
+	int		x;
+	double	camera_x;
+	double	ray_dir_x;
+	double	ray_dir_y;
+
+	x = 0;
+	while (x < WIDTH)
+	{
+		camera_x = (2 * x) / ((double)WIDTH - 1);
+		ray_dir_x = data->dir_x + data->plane_x * camera_x;
+		ray_dir_y = data->dir_y + data->plane_y * camera_x;
+		x++;
+	}
+	cast_ray_next(data);
+}
+
+void	ft_hooks(mlx_key_data_t k, void *param)
+{
+	t_data	*data;
+
+	data = param;
+	k.key = MLX_KEY_ESCAPE;
+	if (mlx_is_key_down(data->mlx, k.key))
+	{
+		mlx_close_window(data->mlx);
+		free(data);
+		exit(EXIT_SUCCESS);
+	}
+	cast_ray(data);
+}
+
+void	init_loop(t_data *data)
+{
+	while (1)
+	{
+		mlx_delete_image(data->mlx, data->img);
+		data->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
+		mlx_image_to_window(data->mlx, data->img, 0, 0);
+		mlx_key_hook(data->mlx, ft_hooks, data);
+		mlx_loop(data->mlx);
+	}
+}
 
 int	init_screen(t_map *map)
 {
-	mlx_t	*mlx;
-	
-	map->n_fd = 0;
-	// double posX = 22, posY = 12;  //x and y start position
-	// double dirX = -1, dirY = 0; //initial direction vector
-	// double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
-	// double time = 0; //time of current frame
-	// double oldTime = 0; //time of previous frame
-	mlx = mlx_init(WIDTH, HEIGHT, "cub3D", true);
-	// init_loop();
-	mlx_terminate(mlx);
+	t_data	*data;
+
+	data->map = map;
+	data->pos_x = map->px;
+	data->pos_y = map->py;
+	data->mlx = mlx_init(WIDTH, HEIGHT, "cub3D", true);
+	init_loop(map);
+	mlx_terminate(data->mlx);
+	// ft_free();
 	return (0);
 }
