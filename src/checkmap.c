1e02feaf3543866742ce19142b1f476be6fd679f
Yuka Itoh <yitoh@f0r1s8.codam.nl>
2024-03-22 20:52:44 +0100

    norm checkmap.c

--- src/checkmap.c
+++ src/checkmap.c
@@ -6,7 +6,7 @@
 /*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/18 17:43:06 by yitoh         #+#    #+#                 */
-/*   Updated: 2024/03/21 11:34:13 by yitoh         ########   odam.nl         */
+/*   Updated: 2024/03/22 20:51:06 by yitoh         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,55 +74,29 @@ int	check_color(char *color, int k)
 
 int	check_nbr(char **tmp, int i, int k, int players)
 {
-	if (tmp[i][k] != '1')
-		return (1);
-	while (tmp[i][k])
+	while (tmp[i])
 	{
-		if ((!tmp[i - 1] || ft_strchr(" \0", tmp[i - 1][k]))
-			&& !ft_strchr("1 \t\n\0", tmp[i][k]))
-			return (ft_printf("up\n"), 1);
-		if ((!tmp[i + 1] || ft_strchr(" \0", tmp[i + 1][k]))
-			&& !ft_strchr("1 \t\n\0", tmp[i][k]))
-			return (ft_printf("down\n"), 1);
-		if ((ft_strchr(" \t\n\0", tmp[i][k + 1]))
-			&& !ft_strchr("1 \n\0", tmp[i][k]))
-			return (ft_printf("right\n"), 1);
-		if (ft_strchr("NSEW", tmp[i][k]))
-			players++;
-		if (!ft_strchr("01NSWE \t\n\0", tmp[i][k]))
-			return (ft_printf("other\n"), 1);
-		k++;
-	}
-	if (players > 1) //need to adjust
-		return (1);
-	return (0);
-}
-
-//remove check of wall around players
-int	check_others(char **tmp, int i, int k, int players)
-{
-	while (i >= 0 && tmp[i])
-	{
-		k = 0;
 		while (tmp[i][k] && ft_strchr(" \t", tmp[i][k]))
 			k++;
-		if (!ft_strchr("\n1", tmp[i][k]) && !players)
-			return (1);
-		if (tmp[i][k] == '1')
+		while (tmp[i][k])
 		{
-			while (tmp[i][k])
-			{
-				if (ft_strchr("NSEW", tmp[i][k]))
-				{
-					players++;
-					if (players > 1)
-						return (1);
-				}
-				k++;
-			}
+			if (((!tmp[i - 1] || ft_strchr(" \0", tmp[i - 1][k])
+					|| !tmp[i + 1] || ft_strchr(" \0", tmp[i + 1][k])
+				|| ft_strchr(" \t\n\0", tmp[i][k + 1]))
+				&& !ft_strchr("1 \t\n\0", tmp[i][k]))
+				|| !ft_strchr("01NSWE \t\n\0", tmp[i][k]))
+				return (ft_printf("map is not properly closed\n"), 1);
+			if (ft_strchr("NSEW", tmp[i][k]))
+				players++;
+			k++;
 		}
-		i--;
+		i++;
+		k = 0;
 	}
+	while (tmp[i - 1][k] && ft_strchr(" \t", tmp[i - 1][k]))
+		k++;
+	if (players > 1 || !ft_strchr("\n1", tmp[i - 1][k]))
+		return (1);
 	return (0);
 }
 
@@ -142,11 +116,13 @@ int	ft_checkmap(char **tmp, int i, int k)
 		else if ((tmp[i][k] == 'F' || tmp[i][k] == 'C')
 				&& (check_color(tmp[i], k + 1)))
 			return (ft_printf("color\n"), 1);
-		else if (!ft_strchr("NSWEFC\n", tmp[i][k]) && check_nbr(tmp, i, k, 0))
-			return (ft_printf("nbr\n"), 1);
+		else if (!ft_strchr("NSWEFC\n", tmp[i][k]))
+		{
+			if (check_nbr(tmp, i, k, 0))
+				return (ft_printf("nbr\n"), 1);
+			break ;
+		}
 		i++;
 	}
-	if (check_others(tmp, i - 1, 0, 0))
-		return (1);
 	return (0);
 }
